848b0732ce8f085b79e4c91dbbb1474854e7074c
rhpamAdmin <>
2019-07-29 10:35:40 +0800

    {/src/main/java/com/myspace/mymortgagedemo/Approval.java}

--- src/main/java/com/myspace/mymortgagedemo/Approval.java
+++ src/main/java/com/myspace/mymortgagedemo/Approval.java
@@ -6,12 +6,35 @@ package com.myspace.mymortgagedemo;
 
 public class Approval implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Approval() {
-    }
+	@org.kie.api.definition.type.Label(value = "Approved ?")
+	private java.lang.Boolean approved;
+	@org.kie.api.definition.type.Label(value = "Approval Message")
+	private java.lang.String message;
 
+	public Approval() {
+	}
 
+	public java.lang.Boolean getApproved() {
+		return this.approved;
+	}
 
+	public void setApproved(java.lang.Boolean approved) {
+		this.approved = approved;
+	}
+
+	public java.lang.String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(java.lang.String message) {
+		this.message = message;
+	}
+
+	public Approval(java.lang.Boolean approved, java.lang.String message) {
+		this.approved = approved;
+		this.message = message;
+	}
 
 }
