c717d960fedcf7b8a405821ad8b865a94f12e941
rhpamAdmin <>
2019-07-29 10:30:25 +0800

    {/src/main/java/com/myspace/mymortgagedemo/ErrorMessage.java}

--- src/main/java/com/myspace/mymortgagedemo/ErrorMessage.java
+++ src/main/java/com/myspace/mymortgagedemo/ErrorMessage.java
@@ -6,12 +6,35 @@ package com.myspace.mymortgagedemo;
 
 public class ErrorMessage implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ErrorMessage() {
-    }
+	@org.kie.api.definition.type.Label(value = "Status")
+	private java.lang.String status;
+	@org.kie.api.definition.type.Label(value = "Messahe")
+	private java.lang.String message;
 
+	public ErrorMessage() {
+	}
 
+	public java.lang.String getStatus() {
+		return this.status;
+	}
 
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public java.lang.String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(java.lang.String message) {
+		this.message = message;
+	}
+
+	public ErrorMessage(java.lang.String status, java.lang.String message) {
+		this.status = status;
+		this.message = message;
+	}
 
 }
